638cb0cfd92658c8b80d074c0e084e9dc000578e
Lauren Hughes <lh0xfb@gmail.com>
2016-01-23 17:50:41 -0600

    simple sculpting

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include "time.h"
 #include <random>
 #include <iostream>
+#include <cfloat>
 
 using namespace std;
 using namespace glm;
@@ -22,8 +23,33 @@ struct Uniforms{
 	vec4 light_pos;
 };
 
-float sphere(const glm::vec3& p, float r){
-	return length(p) - r;
+typedef float (*SDF_Func)(const glm::vec3& c, const glm::vec3& p);
+
+struct CSG{
+	vec3 center;
+	vec3 params;
+	SDF_Func func;
+	int material;
+	CSG(const vec3& c, const vec3& p, SDF_Func f, int m)
+		: center(c), params(p), func(f), material(m){};
+	CSG(const CSG& o)
+		: center(o.center), params(o.params), func(o.func), material(o.material){};
+	CSG& operator = (const CSG& other){
+		center = other.center;
+		params = other.params;
+		func = other.func;
+		material = other.material;
+		return *this;
+	}
+	float operator()(const vec3& p){
+		return func(p - center, params);
+	}
+};
+
+typedef std::vector<CSG> CSGList;
+
+float sphere(const glm::vec3& p, const glm::vec3& b){
+	return length(p) - b.x;
 }
 
 float box(const glm::vec3& p, const glm::vec3& b){
@@ -31,50 +57,66 @@ float box(const glm::vec3& p, const glm::vec3& b){
 	return glm::min(glm::max(d.x, glm::max(d.y, d.z)), 0.0f) + length(glm::max(d, 0.0f));
 }
 
-float map(const glm::vec3& p){
-	//return sphere(p, 0.5f);
-	//return box(p, vec3(1.0f));
-	vector<float> parts;
-	parts.push_back(sphere(p, 0.7f)); // body
-	parts.push_back(sphere(p-vec3(0.7f, 0.1f, 0.0f), 0.4f)); // head
-	parts.push_back(sphere(p-vec3(1.0f, 0.0f, 0.0f), 0.2f)); // snout
-	parts.push_back(sphere(p-vec3(1.1f, 0.2f, 0.1f), 0.05f)); // near eye
-	parts.push_back(sphere(p-vec3(1.1f, 0.2f, -0.1f), 0.05f)); // far eye
-	parts.push_back(sphere(p-vec3(0.9f, 0.4f, 0.2f), 0.1f)); // near ear
-	parts.push_back(sphere(p-vec3(0.9f, 0.4f, -0.2f), 0.1f)); // far ear
-	parts.push_back(sphere(p-vec3(0.4f, -0.4f, 0.4f), 0.2f));	// legs
-	parts.push_back(sphere(p-vec3(0.4f, -0.4f, -0.4f), 0.2f));	// legs
-	parts.push_back(sphere(p-vec3(-0.4f, -0.4f, 0.4f), 0.2f));	// legs
-	parts.push_back(sphere(p-vec3(-0.4f, -0.4f, -0.4f), 0.2f));	// legs
-	parts.push_back(sphere(p-vec3(-0.7f, 0.1f, 0.0f), 0.1f));	// tail
-	float min = parts[0];
-	for(auto i : parts)
-		min = (i < min) ? i : min;
+void getBounds(CSGList& list, vec3& lo, vec3& hi){
+	lo = vec3(FLT_MAX);
+	hi = vec3(FLT_MIN);
+	SDF_Func sph = &sphere;
+	for(auto& i : list){
+		if(i.func == sph){
+			lo = glm::min(lo, i.center - i.params.x);
+			hi = glm::max(hi, i.center + i.params.x);
+		}
+		else{
+			lo = glm::min(lo, i.center - i.params);
+			hi = glm::min(hi, i.center + i.params);
+		}
+	}
+}
+
+float map(const glm::vec3& p, CSGList& list, int& material){
+	float min = FLT_MAX;
+	int mat = -1;
+	for(auto& i : list){
+		float f = i.func(p - i.center, i.params);
+		if(f < min){
+			min = f;
+			mat = i.material;
+		}
+	}
+	material = mat;
 	return min;
 }
 
-void fillCells(VertexBuffer& vb, const glm::vec3& min, const glm::vec3& max, float dim){
+float map(const glm::vec3& p, CSGList& list){
+	float min = FLT_MAX;
+	for(auto& i : list)
+		min = glm::min(min, i.func(p - i.center, i.params));
+	return min;
+}
+
+void fillCells(VertexBuffer& vb, CSGList& list, float spu){
 	vec3 dx(0.01f, 0.0f, 0.0f);
 	vec3 dy(0.0f, 0.01f, 0.0f);
 	vec3 dz(0.0f, 0.0f, 0.01f);
-	vec3 ddim = max - min;
-	vec3 pitch = ddim / dim;
-	float pavg = (pitch.x+pitch.y+pitch.z)*0.3333333f;
-	float psize = pavg * 1024.0f;
-	float threshold = pavg;
-	for(float z = min.z; z <= max.z; z += pitch.z){
-		for(float y = min.y; y <= max.y; y += pitch.y){
-			for(float x = min.x; x <= max.x; x += pitch.x){
+	vec3 min, max;
+	getBounds(list, min, max);
+	float pitch = 1.0f / spu;
+	float psize = 1024.0f * pitch;
+	for(float z = min.z; z <= max.z; z += pitch){
+		for(float y = min.y; y <= max.y; y += pitch){
+			for(float x = min.x; x <= max.x; x += pitch){
 				vec3 i(x, y, z);
-				if(map(i) > threshold)continue;
+				if(map(i, list) > pitch)continue;
 				for(int j = 0; j < 60; j++){
-					float dis = map(i);
+					float dis = map(i, list);
 					vec3 N = normalize(vec3(
-						map(i+dx) - map(i-dx),
-						map(i+dy) - map(i-dy),
-						map(i+dz) - map(i-dz)));
-					if(abs(dis) < threshold*0.001f){
-						vb.push_back(Vertex(i, N, vec3(1.0f, 0.5f, 0.5f), psize));
+						map(i+dx, list) - map(i-dx, list),
+						map(i+dy, list) - map(i-dy, list),
+						map(i+dz, list) - map(i-dz, list)));
+					if(abs(dis) < pitch*0.01f){
+						int mat;
+						map(i, list, mat);
+						vb.push_back(Vertex(i, N, N*0.5f + 0.5f, psize));
 						break;
 					}
 					i -= N * dis;
@@ -118,8 +160,10 @@ int main(int argc, char* argv[]){
     
 	Mesh mesh;
 	
+	CSGList list;
+	
 	VertexBuffer vb;
-	fillCells(vb, vec3(-2.0f), vec3(2.0f), 100.0f);
+	fillCells(vb, list, 25.0f);
 	cout << vb.size() << endl;
 	mesh.upload(vb);
 	vb.clear();
@@ -143,6 +187,13 @@ int main(int argc, char* argv[]){
 		if(glfwGetKey(window.getWindow(), GLFW_KEY_E))
 			uni.light_pos = vec4(camera.getEye(), 0.0f);
 		unibuf.upload(&uni, sizeof(uni));
+		
+		if(glfwGetKey(window.getWindow(), GLFW_KEY_Q)){
+			list.push_back(CSG(camera.getEye()+camera.getAxis(), vec3(0.5f), &sphere, 1));
+			fillCells(vb, list, 25.0f);
+			mesh.upload(vb);
+			vb.clear();
+		}
 		
 		colorProg.bind();
 		timer.begin();
