8efda0f65a963d295ffcd6b98b3f6f3dc4570cf0
Lauren Hughes <zero985@gmail.com>
2016-07-22 04:24:40 -0400

    minor optimizations to octree

--- octree.h
+++ octree.h
@@ -2,42 +2,51 @@
 #define OCTREE_H
 
 #include "csg.h"
+#include "stdlib.h"
 
 #define MINLEN 0.125f
 
 struct OctNode{
-	glm::vec3 center;
+    OctNode* children;
 	float length;
 	float qlen;
+	glm::vec3 center;
 	int depth;
-	bool old;
 	VertexBuffer vb;
 	std::vector<CSG*> items;
-	std::vector<OctNode> children;
+	bool old;
 	// always instantiate root node with depth 0
-	OctNode(const glm::vec3& c, float len=2.0f, int d=0) : center(c), length(len), depth(d), old(false){
-		children.reserve(8);
-		qlen = 1.732051f * len;     // length of vec3 => sqrt(len*len + len*len + len*len) => sqrt(len^2 * 3) => x * sqrt(3)
+	OctNode(const glm::vec3& c, float len=2.0f, int d=0) 
+        : children(nullptr), length(len), center(c), depth(d), old(false){
+        // ratio of cube's side to diagonal: sqrt(len^2 + len^2 + len^2) => sqrt(len^2 * 3) => x * sqrt(3)
+		qlen = 1.732051f * len;
 	}
 	~OctNode(){
-		if(depth == 0){
+		if(!depth){
 			// root node contains all CSG items
 			for(CSG* i : items)
 				delete i;
 		}
+        if(children){
+            for(char i = 0; i < 8; i++)
+                (children + i)->~OctNode();
+            free(children);
+        }
 	}
-	inline bool isLeaf(){return !children.size();}
+	inline bool isLeaf(){return !children;}
 	inline void makeChildren(){
+        if(children)return;
 		const float nlen = length * 0.5f;
 		if(nlen < MINLEN)return;
-		const int ndepth = depth + 1;
-		for(int i = 0; i < 8; i++){
+        children = (OctNode*)malloc(sizeof(OctNode) * 8);
+		for(char i = 0; i < 8; i++){
             glm::vec3 n_c(center);
             n_c.x += (i&4) ? nlen : -nlen;
             n_c.y += (i&2) ? nlen : -nlen;
             n_c.z += (i&1) ? nlen : -nlen;
-			children.push_back(OctNode(n_c, nlen, ndepth));
+            new (children + i) OctNode(n_c, nlen, depth + 1);
 		}
+        
 	}
 	// always pass an item on the heap here
 	inline void insert(CSG* item){
@@ -48,10 +57,11 @@ struct OctNode{
         }
         items.push_back(item);
         old = true;
-        if(this->isLeaf())
-            makeChildren();
-		for(auto& i : children)
-			i.insert(item);
+        makeChildren();
+        if(children){
+            for(char i = 0; i < 8; i++)
+                children[i].insert(item);
+        }
 	}
 	inline void remesh(VertexBuffer& rootvb, float spu){
 		if(isLeaf()){
@@ -63,8 +73,8 @@ struct OctNode{
 			rootvb.insert(rootvb.end(), vb.begin(), vb.end());
 		}
         else{
-            for(OctNode& i : children)
-                i.remesh(rootvb, spu);
+            for(char i = 0; i < 8; i++)
+                children[i].remesh(rootvb, spu);
         }
 	}
 	inline void collectLeaves(std::vector<OctNode*>& list){
@@ -76,8 +86,10 @@ struct OctNode{
 			stack.pop_back();
 			if(cur->isLeaf())
 				list.push_back(cur);
-			for(OctNode& i : cur->children)
-				stack.push_back(&i);
+            else{
+                for(char i = 0; i < 8; i++)
+                    stack.push_back(children + i);
+            }
 		}
 	}
 };
